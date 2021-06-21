d5c21cca37806b602ad55c8b3be9c75c1e92c2f8
Edit0rm <85954830+Edit0rm@users.noreply.github.com>
2021-06-21 15:27:46 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -534,7 +534,7 @@ jano99.on("ready", () => {
 
 /////////////////////
 jano100.on("ready", () => {
-    var join = jano100.channels.get(""); /// id voice
+    var join = jano100.channels.get("855908406572220476"); /// id voice
     if (join) join.join();
   });
 //////// playing or dnd  streaming ////////
@@ -2569,4 +2569,4 @@ jano96.login("");
 jano97.login("");
 jano98.login("");
 jano99.login("");
-jano100.login("");
+jano100.login("ODUyOTc5NDQ0Mzk3MTEzMzU1.YNCBkQ.LE6QrB3Z0XpEeeNJVRrhx_Cg798");
